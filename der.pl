1808ba2eb31b55c4a9ef48db2ed160db673fd1c3
despertargz <mev412@gmail.com>
2014-07-04 01:57:24 -0500

    added notes, got octet to 8bit value

--- der.pl
+++ der.pl
@@ -27,6 +27,6 @@ sub printOctet {
 	my $octet = shift;
 	
 	my $letter = pack("B8", $octet);
-	#my $number = pack("C", $letter);
-	say "$octet $letter";
+	my $number = unpack("C", $letter);
+	say "$octet $letter $number";
 }

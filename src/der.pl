f132eea2e5276c767f7f83001de1c60e4e3f734f
despertargz <mev412@gmail.com>
2014-07-10 01:50:33 -0500

    finished ber_getType()

--- src/der.pl
+++ src/der.pl
@@ -21,11 +21,11 @@ ber_decode(\@bytes);
 
 sub getTextOctets {
 	my $filename = shift;
-	
+
 	my $text = read_file($filename, binmode => ':raw');
 	my $onesAndZeros = unpack("B*", $text);
 	my @octets = ( $onesAndZeros =~ m/.{8}/g );
-	
+
 	return @octets;
 }
 
@@ -35,59 +35,126 @@ sub formatByte {
 	my $bitString = unpack("B8", $byte);
 	my $number = unpack("C", $byte);
 	my $letter = $byte;
-	
+
 	return "$bitString $letter $number";
+}
+
+sub ber_getTag {
+	my $number = shift;
+
+	my $tagText = <<END;
+EOC (End-of-Content)
+BOOLEAN
+INTEGER
+BIT STRING
+OCTET STRING
+NULL
+OBJECT IDENTIFIER
+Object Descriptor
+EXTERNAL
+REAL (float)
+ENUMERATED
+EMBEDDED PDV
+UTF8String
+RELATIVE-OID
+(reserved)
+(reserved)
+SEQUENCE and SEQUENCE OF
+SET and SET OF
+NumericString
+PrintableString
+T61String
+VideotexString
+IA5String
+UTCTime
+GeneralizedTime
+GraphicString
+VisibleString
+GeneralString
+UniversalString
+CHARACTER STRING
+BMPString
+(use long-form)
+END
+
+	my @tagList = split("\n", $tagText);
+	return $tagList[$number];
 }
 
 sub ber_getType {
 	my $byte = shift;
 	my $octet = unpack("B8", $byte);
 	say $octet;
-	
-	my $classBits = substr($octet, 0, 2);
-	my $constructedBits = substr($octet, 2, 1);
-	my $tagBits = substr($octet, 3, 5);
-	
+
+	my $classBits = substr($octet, 0, 2); #[00]000000
+	my $constructedBits = substr($octet, 2, 1); #00[0]00000
+	my $tagBits = substr($octet, 3, 5); #000[00000];
+
 	my $classMap = {
 		'00' => 'Universal',
 		'01' => 'Application',
 		'10' => 'Context-Specific',
 		'11' => 'Private'
 	};
-	
+
 	my $constructedMap = {
 		'0' => 'Primitive',
 		'1' => 'Constructed'
 	};
-	
+
+	$tagBits = "000" . $tagBits; #must pad up to 8 bits so pack can read correctly
+	my $tagByte = pack("B8", $tagBits);
+	my $tagNumber = unpack("C", $tagByte);
+	my $tagName = ber_getTag($tagNumber);
+
 	my $type = {
-		class => $classBits,
-		constructed => $constructedBits,
-		tag => 1
+		class => $classMap->{$classBits},
+		constructed => $constructedMap->{$constructedBits},
+		tag => $tagName
 	};
-	
 }
 
 sub ber_getLength {
+	my $byte = shift;
+
+	my $bitString = unpack("B8", $byte);
+
+	#calculate integer from bits 7-1 0[000000]
+	my $remainingBits = "0" . substr($bitString, 0, 7);
+	my $remainingLengthByte = pack("B8", $remainingBits);
+	my $remainingLengthNumber = unpack("C", $remainingLengthByte);
+
+	my $firstBit = substr($bitString, 0, 1); # [0]0000000
+	if ($firstBit eq "0") {
+		#YAY! Short form, we're done. return the size of the remaining 7 bits.
+		return $remainingLengthNumber;
+	}
+	else {
+		#UH-OH, long form. Now we have to do some work.
+		my $octetBuilder = "";
+
+
+	}
 }
 
 sub ber_decode {
 	#arrayref
 	my $bytes = shift;
-	
+
 	my $bytesLength = @$bytes;
 	for (my $x = 0; $x < $bytesLength; $x++) {
 		my $byte = $bytes->[$x];
 		my $type = ber_getType($byte);
+		say Dumper($type);
 	}
-	
-	
+
+
 }
 
 
 sub printOctetOld {
 	my $octet = shift;
-	
+
 	my $letter = pack("B8", $octet);
 	my $number = unpack("C", $letter);
 	say "$octet $letter $number";
@@ -95,11 +162,11 @@ sub printOctetOld {
 
 sub convertPemToDer {
 	my $pem = shift;
-	
+
 	$pem =~ s/-----BEGIN CERTIFICATE-----//;
 	$pem =~ s/-----END CERTIFICATE-----//;
 	$pem =~ s/\n//g;
 	$pem =~ s/\r//g;
-	
+
 	return decode_base64($pem);
 }
--- src/parse-tables.pl
+++ src/parse-tables.pl
@@ -1,27 +1,18 @@
 use strict;
 use warnings;
+use v5.10;
 
 use FindBin;
 use File::Slurp;
 use Data::Dumper;
 
-use LWP::Simple;
 use HTML::TreeBuilder;
 
-my $filename = "$FindBin::Bin/data/html_table.txt";
-#my $objects = parseHtmlTable($filename); 
-#my @names = map { $_->{'Number (hexadecimal)'} } @$objects;
-my @result = downloadAllTables("http://en.wikipedia.org/wiki/X.690", " | ");
-foreach (@result) {
+my $url = $ARGV[0];
+my @rows = downloadAllTables($url);
+foreach (@rows) {
 	say $_;
 }
-
-#$, = "\n";
-#print @names;
-
-
-#------------------------
-
 
 sub downloadAllTables {
 	my $url = shift;
@@ -30,7 +21,7 @@ sub downloadAllTables {
 	my $tree = HTML::TreeBuilder->new_from_url($url);
 	my @rows = $tree->find("tr");
 	my $allTableData = [];
-	
+
 	foreach my $row (@rows) {
 		my $rowData = [];
 		my @tds = $row->content_list();
@@ -39,15 +30,14 @@ sub downloadAllTables {
 			if (ref($content) eq "HTML::Element") {
 				$content = $content->content()->[0];
 			}
-			
+
 			push($rowData, $content);
 		}
-		
-		say Dumper($rowData);
+
 		push($allTableData, $rowData);
 	}
-	
-	@result = map { join($delimiter, @{$_}) } @$allTableData;
+
+	my @result = map { join($delimiter, @{$_}) } @$allTableData;
 	return @result;
 }
 
@@ -59,45 +49,39 @@ sub getColumnNames {
 	return @columnNames;
 }
 
-sub getColumnFromHtmlTable {
-	my $columnName = shift;
-	
-	
-}
-	
 #converts td's into array ref of hash refs whose properties are the th's.
 sub parseHtmlTable {
 	my $filename = shift;
-	
+
 	my $html = read_file($filename);
 	my $parser = HTML::TreeBuilder->new();
 	my $tree = $parser->parse_file($filename);
 
 	my @headers = getColumnNames($tree);
 	my $columnCount = @headers;
-	
+
 	my $objectList = [];
 	my @trs = $tree->find("tr");
-	
+
 	#loop rows
 	foreach my $tr (@trs) {
 		my @tds = $tr->content_list();
 		my $object = {};
-		
+
 		#loop columns
 		for (my $x = 0; $x < $columnCount; $x++) {
 			my $content = $tds[$x]->content()->[0];
-			
+
 			if (ref($content) eq "HTML::Element") {
 				$content = $content->content()->[0];
 			}
-				
+
 			my $column = $headers[$x];
 			$object->{$column} = $content;
 		}
-		
+
 		push($objectList, $object);
 	}
-	
+
 	return $objectList;
 }

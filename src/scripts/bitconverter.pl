5036c649ac02569b79c2d8b6d3b67db773c58de6
despertargz <mev412@gmail.com>
2014-07-12 01:25:51 -0500

    added bitconverter

---
+++ src/scripts/bitconverter.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use v5.10;
+use File::Slurp;
+
+
+my $filename = shift @ARGV;
+my $bytesString = read_file($filename, binmode => ':raw');
+my @bytes = split //, $bytesString;
+foreach my $byte (@bytes) {
+	my $octet = formatByte($byte);
+	say $octet;
+}
+
+sub formatByte {
+	my $byte = shift;
+
+	my $bitString = unpack("B8", $byte);
+	my $number = unpack("C", $byte);
+	my $letter = $byte;
+
+	return "$bitString $letter $number";
+}
